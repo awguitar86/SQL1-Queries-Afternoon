2fbdb418abcb561cde41af0cb290620272d2ec40
Austin Wright <awguitar86@gmail.com>
2018-02-15 10:01:04 -0700

    completed all problems in TABLE - INVOICE

--- Simple-SQL-Queries.sql
+++ Simple-SQL-Queries.sql
@@ -130,10 +130,25 @@ SELECT COUNT(*) FROM Employee WHERE City = 'Lethbridge';
 
 /* TABLE - INVOICE */
 -- 1. Count how many orders were made from the USA.
+SELECT COUNT(*) FROM Invoice WHERE BillingCountry = 'USA';
+
 -- 2. Find the largest order total amount.
+SELECT MAX(Total) FROM Invoice;
+
 -- 3. Find the smallest order total amount.
+SELECT MIN(Total) FROM Invoice;
+
 -- 4. Find all orders bigger than $5.
+SELECT * FROM Invoice WHERE Total > 5;
+
 -- 5. Count how many orders were smaller than $5.
+SELECT COUNT(*) FROM Invoice WHERE Total < 5;
+
 -- 6. Count how many orders were in CA, TX, or AZ (use IN).
+SELECT COUNT(*) FROM Invoice WHERE BillingState IN ('CA', 'TX', 'AZ');
+
 -- 7. Get the average total of the orders.
+SELECT AVG(Total) FROM Invoice;
+
 -- 8. Get the total sum of the orders.
+SELECT SUM(Total) FROM Invoice;
